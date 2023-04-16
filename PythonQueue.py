e9f40b41d5788e8ef090403c8d6972672c8ef322
McKaylaMik <124284113+McKaylaMik@users.noreply.github.com>
2023-04-16 15:12:48 -0400

    Add files via upload

---
+++ PythonQueue.py
@@ -0,0 +1,29 @@
+# Python program to  
+# demonstrate queue implementation 
+# using list 
+  
+# Initializing a queue 
+names = [] 
+  
+# Adding elements to the queue
+acceptedName = ['Joe', 'Sally', 'Jim', 'June', 'Bill', 'Betty', 'Sara', 'Zak', 'Anne', 'Kate']
+for x in range(10):
+    name = input("Enter name: ")
+    names.append(name)
+    
+print ("Queues are First-In First-Out (FIFO) data structures") 
+print("\nInitial names. Used names.append(item) to enter elements into the queue") 
+print(names) 
+  
+# Removing elements from the queue 
+print("\nElements dequeued from queue. Used names.pop(0) to pop element in front") 
+for x in range(10):
+    print(names.pop(0))
+  
+print("\nQueue after removing elements") 
+print(names) 
+  
+# Uncommenting print(queue.pop(0)) 
+# will raise and IndexError 
+# as the queue is now empty 
+
